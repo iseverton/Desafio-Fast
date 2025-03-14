b6da25e2319dfff093c542a9356ab628efb01ea3
iseverton <iseverton2024@gmail.com>
2025-03-14 16:21:56 -0300

    feat: add Employee and Workshop models

---
+++ WorkshopManager/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkshopManager.Api.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ WorkshopManager/Models/Workshop.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkshopManager.Api.Entities
+{
+    public class Workshop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public DateTime EventDate { get; set; }
+        
+    }
+}
