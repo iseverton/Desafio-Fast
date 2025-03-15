5bb7a1022b9396c536ca43baad4c5296b38a1af3
iseverton <iseverton2024@gmail.com>
2025-03-15 15:07:57 -0300

    feat: add mappings

---
+++ WorkshopManager/Mappings/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using WorkshopManager.Api.DTOs.Employee;
+using WorkshopManager.Api.Entities;
+
+namespace WorkshopManager.Api.Mappings;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Employee, EmployeeCreateDTO>().ReverseMap();
+        CreateMap<Employee, EmployeeResponseDTO>().ReverseMap();
+        CreateMap<EmployeeUpdateDTO, Employee>();
+    }
+}
