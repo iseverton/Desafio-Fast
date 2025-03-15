9f40d2b28fd62aefcc1abb47b7398a2e6bad68fd
iseverton <iseverton2024@gmail.com>
2025-03-15 15:22:14 -0300

    feat: implement EmployeeController

--- WorkshopManager/Controllers/EmployeeController.cs
+++ WorkshopManager/Controllers/EmployeeController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+using WorkshopManager.Api.DTOs.Employee;
+using WorkshopManager.Api.Services.Interfaces;
 
 namespace WorkshopManager.Api.Controllers;
 
@@ -6,4 +10,89 @@ namespace WorkshopManager.Api.Controllers;
 [Route("api/[controller]")]
 public class EmployeeController : ControllerBase
 {
+    private readonly IEmployeeService _employeeService;
+
+    public EmployeeController(IEmployeeService employeeService)
+    {
+        _employeeService = employeeService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetEmployees()
+    {
+        try
+        {
+            var result = await _employeeService.GetAll();
+            if (result.IsSucceeded) return Ok(result);
+            return BadRequest(result);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetEmployeeById(int id)
+    {
+        try
+        {
+            var result = await _employeeService.GetById(id);
+            if(result.IsSucceeded) return Ok(result);
+            return BadRequest(result);
+        }
+        catch(Exception e )
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPost]
+    public async Task <IActionResult> CreateEmployee(EmployeeCreateDTO employeeCreateDTO)
+    {
+        try
+        {
+            var result = await _employeeService.PostEmployeeAsync(employeeCreateDTO);
+            if (result.IsSucceeded) {
+                return CreatedAtAction(nameof(GetEmployeeById), new { id = result }, result);
+            }
+            return BadRequest(result);
+
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteEmployee(int id)
+    {
+        try
+        {
+            var result = await _employeeService.DeleteEmployee(id);
+            if (result.IsSucceeded) return NoContent();
+            return BadRequest(result);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateEmployee(int id,EmployeeUpdateDTO employeeUpdateDTO)
+    {
+        try
+        {
+            var result = await _employeeService.UpdateEmployee(id,employeeUpdateDTO);
+            if (result.IsSucceeded) return Ok(result);
+            return BadRequest(result);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
 }
