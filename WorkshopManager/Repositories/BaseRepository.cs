f897171b3402add1aaa8029f5810af4dd2615f6c
iseverton <iseverton2024@gmail.com>
2025-03-14 21:37:18 -0300

    feat: add IBaseRepository and BaseReposity repository

---
+++ WorkshopManager/Repositories/BaseRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorkshopManager.Api.Data.Context;
+using WorkshopManager.Api.Repositories.Interfaces;
+
+namespace WorkshopManager.Api.Repositories;
+
+public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+{
+    private readonly AppDbContext _context;
+    private readonly DbSet<TEntity> _dbSet;
+
+    public BaseRepository(AppDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<TEntity>();
+    }
+
+    public Task<TEntity> AddAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<TEntity>> GetAllAsync()
+    {
+        return await _dbSet.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<TEntity> GetByIdAsync(int id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task RemoveAsync(TEntity entity)
+    {
+        _dbSet.Remove(entity);
+        await _context.SaveChangesAsync();
+
+    }
+
+    public Task<TEntity> SearchByConditionAsync(System.Linq.Expressions.Expression<Func<TEntity, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ WorkshopManager/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace WorkshopManager.Api.Repositories.Interfaces;
+
+public interface IBaseRepository<TEntity> where TEntity : class
+{
+    Task<IEnumerable<TEntity>> GetAllAsync();
+    Task<TEntity> GetByIdAsync(int id);
+    Task<TEntity> SearchByConditionAsync(Expression<Func<TEntity, bool>> predicate);
+
+    Task<TEntity> AddAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task RemoveAsync(TEntity entity);
+}
