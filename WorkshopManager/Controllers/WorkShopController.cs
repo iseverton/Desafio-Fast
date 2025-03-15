c2e841d42aaac2750edd1784cd7b34e428a900ff
iseverton <iseverton2024@gmail.com>
2025-03-14 21:11:20 -0300

    feat: add EmployeController and WorkShopController

---
+++ WorkshopManager/Controllers/WorkShopController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorkshopManager.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WorkShopController : ControllerBase
+{
+    
+
+}
